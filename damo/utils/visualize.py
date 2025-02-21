b3f82aa3417288a8bac758ab48e3cef08da85ed2
Bin <1909916440@qq.com>
2025-02-21 11:55:36 +0800

    fix bug 'img marked as output argument, but provided NumPy array marked as readonly'

--- damo/utils/visualize.py
+++ damo/utils/visualize.py
@@ -9,7 +9,8 @@ __all__ = ['vis']
 
 def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
 
-    for i in range(len(boxes)):
+    img_copy = np.copy(img)
+    for i in range(len(boxes)):   
         box = boxes[i]
         cls_id = int(cls_ids[i])
         score = scores[i]
@@ -27,21 +28,22 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
         font = cv2.FONT_HERSHEY_SIMPLEX
 
         txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
-        cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+        
+        cv2.rectangle(img_copy, (x0, y0), (x1, y1), color, 2)
 
         txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
-        cv2.rectangle(img, (x0, y0 + 1),
-                      (x0 + txt_size[0] + 1, y0 + int(1.5 * txt_size[1])),
-                      txt_bk_color, -1)
-        cv2.putText(img,
+        cv2.rectangle(img_copy, (x0, y0 + 1),
+                     (x0 + txt_size[0] + 1, y0 + int(1.5 * txt_size[1])),
+                     txt_bk_color, -1)
+        
+        cv2.putText(img_copy,
                     text, (x0, y0 + txt_size[1]),
                     font,
                     0.4,
                     txt_color,
                     thickness=1)
-
-    return img
-
+        
+    return img_copy
 
 _COLORS = np.array([
     0.000, 0.447, 0.741, 0.850, 0.325, 0.098, 0.929, 0.694, 0.125, 0.494,
